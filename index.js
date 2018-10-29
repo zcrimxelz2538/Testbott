88ae24fb621dced29b32a2b0622a5e5316870620
ZcrimXelz <neyneznf@gmail.com>
2018-10-29 22:25:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const bodyParser = require('body-parser');
 server()
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({ extended: false}))
-    .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${ PORT }`))
+    .get('/', (req, res) => res.send(`Hi 5ere! This is a nodejs-line-api running on PORT: ${ PORT }`))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));	
